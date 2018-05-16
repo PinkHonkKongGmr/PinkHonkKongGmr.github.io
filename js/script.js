08ac064312ec1db10a13a919f6afbb3a7c15e903
PinkHonkKongGmr <39237301+PinkHonkKongGmr@users.noreply.github.com>
2018-05-16 22:25:54 +0300

    bug was exterminate
    устранил баг!!1
    ещё мама говорила чтоб я не пихал свои рекурсии во все подряд функции.

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ var go = document.getElementById('go');
 var modal=document.getElementById('modal');
 var start=document.getElementById('start');
 
+
 go.addEventListener('click',function () {
   modal.style.display="block";
   start.style.display="none";
-  BigMath();
+ BigMath();
 
 });
 
 function BigMath() {
 
 
-var right;
+
+
 var numarray1 =[];
 var numarray2=[];
 var num1=document.getElementById('num1');
@@ -38,9 +40,7 @@ var o =imp.style.display;
 
 
 ctx.strokeStyle = 'rgba(248,23,62,0.7)';
-// var right = 1;
 
-// function arraytest() {
 
   for (var i = 6; i <10; i++) {
   numarray1.push(i);
@@ -50,9 +50,17 @@ for (var x = 2; x < 9; x++) {
   numarray2.push(x);
 };
 
+function getNumOne() {
+
+
 var rand1 = Math.floor(Math.random() * numarray1.length);
-var randomnum1 =numarray1[rand1];
-// var randomnum1 =6;
+var r1 =numarray1[rand1];
+return r1;
+}
+
+
+
+var randomnum1 = getNumOne();
 
 
 function getNumTwo() {
@@ -84,7 +92,7 @@ mo1.innerText=randomnum1;
 mo3.innerText=nm2valid;
 // return randomnum1;
 
-function erase() {
+function reborn() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 num.style.display="block";
 yeah.innerText="";
@@ -95,11 +103,20 @@ imp.value="";
 result.value="";
 sum.style.display="block";
 sum.innerText="?";
-thissum=null;
-nm2valid=null;
+
+randomnum1=getNumOne();
+nm2valid = getNumTwo();
+thissum = randomnum1 + nm2valid;
+
+num1.innerText=randomnum1;
+num2.innerText=nm2valid;
+mo1.innerText=randomnum1;
+mo3.innerText=nm2valid;
+leftCanvasDraw();
 
 
-BigMath();
+
+
 
 
 };
@@ -261,718 +278,7 @@ else if (randomnum1==9&&thissum==14) {
 
 
 
-// function canvasAdder(a) {
-//
-//
-//
-//
-//   if (randomnum1==6) {
-//
-//     num.style.top="219px";
-//     yeah.style.top="211px";
-//
-//      if (thissum==11) {
-//
-//        imp.style.top="227px";
-//        yeah2.style.top="218px";
-//
-//        imp.style.left="475px";
-//        yeah2.style.left="486px";
-//
-//
-//
-//
-//        ctx.beginPath();
-//        ctx.moveTo(218,200);
-//        ctx.lineTo(223,190);
-//        ctx.lineWidth = 2;
-//
-//        ctx.stroke();
-//
-//        ctx.beginPath();
-//        ctx.moveTo(218,200);
-//        ctx.lineTo(213,190);
-//        ctx.lineWidth = 2;
-//
-//        ctx.stroke();
-//
-//        ctx.beginPath();
-//        ctx.arc(baseX,200,62,0, Math.PI,true);
-//        ctx.lineWidth = 4;
-//
-//        ctx.stroke();
-//
-//      if (thissum==a) {
-//
-//
-//        ctx.beginPath();
-//        ctx.arc(269,200,51,0, Math.PI,true);
-//        ctx.lineWidth = 4;
-//
-//        ctx.stroke();
-//
-//        ctx.beginPath();
-//        ctx.moveTo(320,200);
-//        ctx.lineTo(325,190);
-//        ctx.lineWidth = 2;
-//
-//        ctx.stroke();
-//
-//        ctx.beginPath();
-//        ctx.moveTo(320,200);
-//        ctx.lineTo(315,190);
-//        ctx.lineWidth = 2;
-//
-//        ctx.stroke();
-//        }
-//
-//      }
-//
-//   else if (thissum==12)
-//         {
-//
-//           imp.style.top="221px";
-//           yeah2.style.top="212px";
-//
-//           imp.style.left="483px";
-//           yeah2.style.left="490px";
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(223,190);
-//           ctx.lineWidth = 2;
-//
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(213,190);
-//           ctx.lineWidth = 2;
-//
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.arc(156,200,62,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//           ctx.stroke();
-//
-//          if (thissum==a) {
-//           ctx.beginPath();
-//           ctx.arc(280,200,61,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(341,200);
-//           ctx.lineTo(346,190);
-//           ctx.lineWidth = 2;
-//
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(341,200);
-//           ctx.lineTo(336,190);
-//           ctx.lineWidth = 2;
-//
-//           ctx.stroke();
-//         }
-//         else {
-//           return false
-//         }
-//
-//
-//
-//         }
-//         else if (thissum==13) {
-//
-//           imp.style.top="210px";
-//           yeah2.style.top="202px";
-//
-//           imp.style.left="494px";
-//           yeah2.style.left="499px";
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(223,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(213,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.arc(156,200,62,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//           ctx.stroke();
-//
-//               if (thissum==a) {
-//           ctx.beginPath();
-//           ctx.arc(289,200,72,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(361,200);
-//           ctx.lineTo(366,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(361,200);
-//           ctx.lineTo(356,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();}
-//
-//
-//
-//         }
-//
-//         else if (thissum==14) {
-//
-//           imp.style.top="201px";
-//           yeah2.style.top="192px";
-//
-//           imp.style.left="503px";
-//           yeah2.style.left="510px";
-//
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(223,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(218,200);
-//           ctx.lineTo(213,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.arc(156,200,62,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//           ctx.stroke();
-//
-//
-//           if (thissum==a) {
-//
-//
-//           ctx.beginPath();
-//           ctx.arc(300,200,82,0, Math.PI,true);
-//           ctx.lineWidth = 4;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(382,200);
-//           ctx.lineTo(387,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//
-//           ctx.beginPath();
-//           ctx.moveTo(382,200);
-//           ctx.lineTo(377,190);
-//           ctx.lineWidth = 2;
-//           ctx.stroke();
-//         }
-//
-//
-//
-//         }
-//       }
-//
-//   else if (randomnum1==7) {
-//
-//     num.style.left="363px";
-//     num.style.top="205px";
-//
-//     yeah.style.left="363px";
-//     yeah.style.top="203px";
-//
-//     if (thissum==11) {
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(232,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(242,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.arc(baseX+11,200,72,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//          if (thissum==a) {
-//
-//       ctx.beginPath();
-//       ctx.arc(280,200,40,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(320,200);
-//       ctx.lineTo(325,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(320,200);
-//       ctx.lineTo(315,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();}
-//
-//
-//     }
-//
-//     else if (thissum==12) {
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(232,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(242,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.arc(165,200,72,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//               if (thissum==a) {
-//       ctx.beginPath();
-//       ctx.arc(290,200,50,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(340,200);
-//       ctx.lineTo(345,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(340,200);
-//       ctx.lineTo(335,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();}
-//
-//     }
-//
-//     else if (thissum==13) {
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(232,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(242,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.arc(165,200,72,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//         if (thissum==a) {
-//       ctx.beginPath();
-//       ctx.arc(301,200,62,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(363,200);
-//       ctx.lineTo(368,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(363,200);
-//       ctx.lineTo(358,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();}
-//
-//     }
-//
-//     else if (thissum==14) {
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(232,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(237,200);
-//       ctx.lineTo(242,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.arc(165,200,72,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//           if (thissum==a) {
-//       ctx.beginPath();
-//       ctx.arc(311,200,72,0, Math.PI,true);
-//       ctx.lineWidth = 4;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(383,200);
-//       ctx.lineTo(388,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();
-//
-//       ctx.beginPath();
-//       ctx.moveTo(383,200);
-//       ctx.lineTo(378,190);
-//       ctx.lineWidth = 2;
-//       ctx.stroke();}
-//
-//     }
-//
-//   }
-//
-//   else if (randomnum1==8) {
-//
-//     num.style.top="197px";
-//     num.style.left="370px";
-//
-//     yeah.style.left="375px";
-//     yeah.style.top="188px";
-//
-//   if (thissum==11) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(252,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(262,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(baseX+21,200,82,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//          if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(290,200,31,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(321,200);
-//     ctx.lineTo(326,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(321,200);
-//     ctx.lineTo(316,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//   else if (thissum==12) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(252,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(262,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(175,200,82,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//         if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(300,200,41,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(341,200);
-//     ctx.lineTo(346,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(341,200);
-//     ctx.lineTo(336,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//
-//   else if (thissum==13) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(252,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(262,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(175,200,82,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//            if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(310,200,51,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(361,200);
-//     ctx.lineTo(366,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(361,200);
-//     ctx.lineTo(356,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//
-//   }
-//   else if (thissum==14) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(252,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(257,200);
-//     ctx.lineTo(262,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(175,200,82,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//            if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(322,200,62,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(384,200);
-//     ctx.lineTo(389,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(384,200);
-//     ctx.lineTo(379,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//
-//   }
-//
-//   else if (randomnum1==9)
-//    {
-//      num.style.left="385px";
-//      yeah.style.left="385px";
-//   if (thissum=="11") {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(274,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(284,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(baseX+30,200,93,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//           if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(300,200,21,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(320,200);
-//     ctx.lineTo(325,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(320,200);
-//     ctx.lineTo(315,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//   else if (thissum==12) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(274,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(284,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(186,200,93,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     if (thissum==a) {
-//
-//     ctx.beginPath();
-//     ctx.arc(310,200,31,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(341,200);
-//     ctx.lineTo(346,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(341,200);
-//     ctx.lineTo(336,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//   else if (thissum==13) {
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(274,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(284,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(186,200,93,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//         if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(320,200,41,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(361,200);
-//     ctx.lineTo(366,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(361,200);
-//     ctx.lineTo(356,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//   }
-//
-//   else if (thissum==14) {
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(274,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(279,200);
-//     ctx.lineTo(284,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.arc(186,200,93,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//   if (thissum==a) {
-//     ctx.beginPath();
-//     ctx.arc(331,200,51,0, Math.PI,true);
-//     ctx.lineWidth = 4;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(382,200);
-//     ctx.lineTo(387,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();
-//
-//     ctx.beginPath();
-//     ctx.moveTo(382,200);
-//     ctx.lineTo(377,190);
-//     ctx.lineWidth = 2;
-//     ctx.stroke();}
-//
-//
-//   }
-//   }
-//
-//
-//
-// };
-//
-// canvasAdder(120);
+
 
 num.addEventListener('keyup',function () {
   if (num.value==randomnum1) {
@@ -1031,7 +337,7 @@ result.addEventListener('keyup',function () {
 
       sum.style.display="block";
       btn2.style.display="block";
-    
+
 
 
 
@@ -1044,7 +350,8 @@ result.addEventListener('keyup',function () {
 
     yes.addEventListener('click',function () {
 
-     erase();
+     reborn();
+
 
     });
 
