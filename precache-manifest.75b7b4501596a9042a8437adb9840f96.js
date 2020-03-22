ea76ae770c7bdee8063ecea8d17ed55e571f56b9
Vladimir Nickolaev <exlnted@gmail.com>
2020-03-22 23:37:51 +0300

    ok

---
+++ precache-manifest.75b7b4501596a9042a8437adb9840f96.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad41bdfe928615e0057ec82b257f07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c226680d4aa4f42695",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "9dafd32de711631bb37d",
+    "url": "/static/css/main.f1dfee36.chunk.css"
+  },
+  {
+    "revision": "40c226680d4aa4f42695",
+    "url": "/static/js/2.e9e237c9.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.e9e237c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dafd32de711631bb37d",
+    "url": "/static/js/main.461d96e1.chunk.js"
+  },
+  {
+    "revision": "bfc8ac0f48776f4c4062",
+    "url": "/static/js/runtime-main.89e81120.js"
+  },
+  {
+    "revision": "15cb847a5a09f81988cd8317e070ee6c",
+    "url": "/static/media/chicken.15cb847a.jpg"
+  },
+  {
+    "revision": "8d5914d4b057a271e54641b72aaa16f5",
+    "url": "/static/media/cow.8d5914d4.jpg"
+  },
+  {
+    "revision": "ff8e83f946508bd59a7de7b253872dac",
+    "url": "/static/media/kur.ff8e83f9.jpg"
+  },
+  {
+    "revision": "94db500a834e1d9ff20393297e90117a",
+    "url": "/static/media/pshenichnoepole.94db500a.jpg"
+  },
+  {
+    "revision": "ff9ee3bcb73f86a8758093188e790ddb",
+    "url": "/static/media/pustpole.ff9ee3bc.jpg"
+  }
+]);
