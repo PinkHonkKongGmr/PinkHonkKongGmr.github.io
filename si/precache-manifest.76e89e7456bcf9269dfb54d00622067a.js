196ec39d84ae5f31aee79cc402a682cab6e40951
Vladimir Nickolaev <exlnted@gmail.com>
2020-04-14 19:00:53 +0300

    add learning interface

---
+++ si/precache-manifest.76e89e7456bcf9269dfb54d00622067a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b14848b0a6b129b9af391d553ed7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6afe635bbe1df4bf8676",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5d33b6213bf683350252",
+    "url": "/static/css/main.788363e2.chunk.css"
+  },
+  {
+    "revision": "6afe635bbe1df4bf8676",
+    "url": "/static/js/2.b228530a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b228530a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d33b6213bf683350252",
+    "url": "/static/js/main.fef636cf.chunk.js"
+  },
+  {
+    "revision": "4b16a235b37353b0151b",
+    "url": "/static/js/runtime-main.1fd7bb01.js"
+  }
+]);
