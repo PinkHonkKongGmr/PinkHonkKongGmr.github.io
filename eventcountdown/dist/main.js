155b94b47f75658ca32f06ad90623a14541f7e65
Vladimir Nickolaev <exlnted@gmail.com>
2020-05-06 15:05:15 +0300

    add countdown project

---
+++ eventcountdown/dist/main.js
@@ -0,0 +1,197 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/countdown.js":
+/*!**************************!*\
+  !*** ./src/countdown.js ***!
+  \**************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nclass CountDown {\r\n\tconstructor(dayValue, mounthValue, yearValue) {\r\n\t\tthis.dayValue = dayValue;\r\n\t\tthis.mounthValue = mounthValue;\r\n\t\tthis.yearValue = yearValue;\r\n\t\tthis.date = new Date(`${this.dayValue} ${this.mounthValue} ${this.yearValue}`);\r\n\t}\r\n\tgetDate() {\r\n\t\treturn this.date - Date.now();\r\n\t}\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (CountDown);\r\n\n\n//# sourceURL=webpack:///./src/countdown.js?");
+
+/***/ }),
+
+/***/ "./src/domconstructor.js":
+/*!*******************************!*\
+  !*** ./src/domconstructor.js ***!
+  \*******************************/
+/*! exports provided: generateSelectorBlock, generateSelector, generateOptions, generateResultWrapper */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateSelectorBlock\", function() { return generateSelectorBlock; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateSelector\", function() { return generateSelector; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateOptions\", function() { return generateOptions; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateResultWrapper\", function() { return generateResultWrapper; });\n/* harmony import */ var _selectActions__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./selectActions */ \"./src/selectActions.js\");\n/* harmony import */ var _year__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./year */ \"./src/year.js\");\n\r\n\r\n\r\nconst generateSelectorBlock = function () {\r\n\tconst yearInstance = new _year__WEBPACK_IMPORTED_MODULE_1__[\"default\"]().year;\r\n\r\n\tconst wrapper = document.createElement('div');\r\n\twrapper.classList.add('form-row', 'form-group');\r\n\r\n\tconst startBtn = document.createElement('button');\r\n\tstartBtn.classList.add('btn', 'btn-primary');\r\n\tstartBtn.textContent = 'Запуск';\r\n\r\n\tconst removeBtn = document.createElement('button');\r\n\tremoveBtn.classList.add('btn', 'btn-primary');\r\n\tremoveBtn.textContent = 'Удолить';\r\n\r\n\tconst wait = document.createElement('div');\r\n\r\n\tconst selectDays = new _selectActions__WEBPACK_IMPORTED_MODULE_0__[\"default\"](generateSelector('disabled'));\r\n\tconst selectMounths = new _selectActions__WEBPACK_IMPORTED_MODULE_0__[\"default\"](generateSelector('enabled'));\r\n\r\n\tconst dayWrapper = document.createElement('div');\r\n\tdayWrapper.classList.add('form-group', 'col-md-1');\r\n\tdayWrapper.appendChild(selectDays.getInstance());\r\n\r\n\tconst mounthWrapper = document.createElement('div');\r\n\tmounthWrapper.classList.add('form-group', 'col-md-8');\r\n\tmounthWrapper.appendChild(selectMounths.getInstance());\r\n\r\n\tconst yearInput = document.createElement('input');\r\n\tyearInput.classList.add('form-control');\r\n\tyearInput.setAttribute('type', 'number');\r\n\tyearInput.value = yearInstance.year;\r\n\r\n\tconst yearWrapper = document.createElement('div');\r\n\tyearWrapper.classList.add('form-group', 'col-md-3');\r\n\tyearWrapper.appendChild(yearInput);\r\n\r\n\t[dayWrapper, mounthWrapper, yearWrapper, startBtn, removeBtn, wait].forEach((child) => wrapper.appendChild(child));\r\n\r\n\treturn { yearInstance, selectDays, selectMounths, yearInput, startBtn, removeBtn, wait, wrapper };\r\n};\r\n\r\nconst generateSelector = function (disableStatus) {\r\n\tconst selectorDays = document.createElement('select');\r\n\tselectorDays.classList.add('form-control');\r\n\tselectorDays.setAttribute(disableStatus, true);\r\n\treturn selectorDays;\r\n};\r\n\r\nconst generateOptions = function (option, content) {\r\n\tlet newElement = document.createElement(option);\r\n\tnewElement.textContent = content;\r\n\treturn newElement;\r\n};\r\n\r\nconst generateResultWrapper = function (result, block, btn, interval) {\r\n\tlet resultContainer = document.createElement('div');\r\n\tbtn.setAttribute('disabled', true);\r\n\tif (result !== null) {\r\n\t\tresultContainer.textContent = result.seconds;\r\n\t} else {\r\n\t\tclearInterval(interval);\r\n\t\tsetTimeout(() => {\r\n\t\t\tblock.textContent = '';\r\n\t\t\tbtn.removeAttribute('disabled');\r\n\t\t}, 1300);\r\n\t\tresultContainer.textContent = 'пора отпустить прошлое!';\r\n\t}\r\n\treturn resultContainer;\r\n};\r\n\n\n//# sourceURL=webpack:///./src/domconstructor.js?");
+
+/***/ }),
+
+/***/ "./src/eventblock.js":
+/*!***************************!*\
+  !*** ./src/eventblock.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _mounths__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./mounths */ \"./src/mounths.js\");\n/* harmony import */ var _helpers__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./helpers */ \"./src/helpers.js\");\n/* harmony import */ var _domconstructor__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./domconstructor */ \"./src/domconstructor.js\");\n/* harmony import */ var _countdown__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./countdown */ \"./src/countdown.js\");\n/* harmony import */ var _timeFormatter__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./timeFormatter */ \"./src/timeFormatter.js\");\n\r\n\r\n\r\n\r\n\r\n\r\nfunction EventBlock() {\r\n\tlet interval = null;\r\n\tconst Block = Object(_domconstructor__WEBPACK_IMPORTED_MODULE_2__[\"generateSelectorBlock\"])();\r\n\tthis.createBlock = () => {\r\n\t\tconst renderDays = (val) => {\r\n\t\t\tif (val.length > 0) {\r\n\t\t\t\tBlock.selectDays.addOptions(\r\n\t\t\t\t\tObject(_helpers__WEBPACK_IMPORTED_MODULE_1__[\"arrayCreator\"])(_mounths__WEBPACK_IMPORTED_MODULE_0__[\"default\"].getMounth(val).daysPerMounth(Block.yearInstance.leap))\r\n\t\t\t\t);\r\n\t\t\t\tBlock.selectDays.switchDisable();\r\n\t\t\t}\r\n\t\t};\r\n\r\n\t\tBlock.yearInput.addEventListener('input', (e) => {\r\n\t\t\tBlock.yearInstance.year = e.target.value;\r\n\t\t\trenderDays(Block.selectMounths.getInstance().value);\r\n\t\t});\r\n\r\n\t\tBlock.selectMounths.getInstance().addEventListener('change', (e) => renderDays(e.target.value));\r\n\t\tBlock.selectMounths.addOptions(Object.entries(_mounths__WEBPACK_IMPORTED_MODULE_0__[\"default\"].mounths).map((mounth) => mounth[1].name));\r\n\r\n\t\tBlock.startBtn.addEventListener('click', (e) => {\r\n\t\t\te.preventDefault();\r\n\t\t\tlet cd = new _countdown__WEBPACK_IMPORTED_MODULE_3__[\"default\"](\r\n\t\t\t\tBlock.selectDays.getInstance().value,\r\n\t\t\t\t_mounths__WEBPACK_IMPORTED_MODULE_0__[\"default\"].getKey(Block.selectMounths.getInstance().value),\r\n\t\t\t\tBlock.yearInstance.year\r\n\t\t\t);\r\n\r\n\t\t\tinterval = setInterval(() => {\r\n\t\t\t\t// для того, чтобы мы могли стилизовать вывод красиво\r\n\t\t\t\tBlock.wait.innerHTML = '';\r\n\t\t\t\tBlock.wait.appendChild(\r\n\t\t\t\t\tObject(_domconstructor__WEBPACK_IMPORTED_MODULE_2__[\"generateResultWrapper\"])(Object(_timeFormatter__WEBPACK_IMPORTED_MODULE_4__[\"default\"])(cd.getDate()), Block.wait, Block.startBtn, interval)\r\n\t\t\t\t);\r\n\t\t\t}, 100);\r\n\t\t});\r\n\r\n\t\treturn Block.wrapper;\r\n\t};\r\n\r\n\tthis.deleteBlock = (deleteFunction) => {\r\n\t\tBlock.removeBtn.addEventListener('click', (e) => {\r\n\t\t\te.preventDefault();\r\n\t\t\tif (interval !== null) {\r\n\t\t\t\tclearInterval(interval);\r\n\t\t\t}\r\n\t\t\tdeleteFunction();\r\n\t\t});\r\n\t};\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (EventBlock);\r\n\n\n//# sourceURL=webpack:///./src/eventblock.js?");
+
+/***/ }),
+
+/***/ "./src/helpers.js":
+/*!************************!*\
+  !*** ./src/helpers.js ***!
+  \************************/
+/*! exports provided: arrayCreator */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"arrayCreator\", function() { return arrayCreator; });\nconst arrayCreator = function (len) {\r\n\tconst arr = [];\r\n\tfor (let i = 0; i < len; i++) {\r\n\t\tarr.push(i + 1);\r\n\t}\r\n\treturn arr;\r\n};\r\n\n\n//# sourceURL=webpack:///./src/helpers.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _eventblock__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./eventblock */ \"./src/eventblock.js\");\n\r\n\r\nconst root = document.getElementById('root');\r\nconst addBtn = document.getElementById('add');\r\n\r\nconst deleteBlock = (block) => root.removeChild(block);\r\n\r\naddBtn.addEventListener('click', (e) => {\r\n\te.preventDefault();\r\n\tconst eventBlock = new _eventblock__WEBPACK_IMPORTED_MODULE_0__[\"default\"]();\r\n\tconst newBlock = eventBlock.createBlock();\r\n\troot.appendChild(newBlock);\r\n\tconst deleteBlockBind = deleteBlock.bind(null, newBlock);\r\n\teventBlock.deleteBlock(deleteBlockBind);\r\n});\r\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/mounths.js":
+/*!************************!*\
+  !*** ./src/mounths.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nclass Mounths {\r\n\tconstructor() {\r\n\t\tthis.mounths = {\r\n\t\t\tJanuary: {\r\n\t\t\t\tname: 'Январь',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tFebruary: {\r\n\t\t\t\tname: 'Февраль',\r\n\t\t\t\tdaysPerMounth: (leap) => (leap ? 29 : 28),\r\n\t\t\t},\r\n\t\t\tMarch: {\r\n\t\t\t\tname: 'Март',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tApril: {\r\n\t\t\t\tname: 'Апрель',\r\n\t\t\t\tdaysPerMounth: () => 30,\r\n\t\t\t},\r\n\t\t\tMay: {\r\n\t\t\t\tname: 'Май',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tJune: {\r\n\t\t\t\tname: 'Июнь',\r\n\t\t\t\tdaysPerMounth: () => 30,\r\n\t\t\t},\r\n\t\t\tJuly: {\r\n\t\t\t\tname: 'Июль',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tAugust: {\r\n\t\t\t\tname: 'Август',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tSeptember: {\r\n\t\t\t\tname: 'Сентябрь горит',\r\n\t\t\t\tdaysPerMounth: () => 30,\r\n\t\t\t},\r\n\t\t\tOctober: {\r\n\t\t\t\tname: 'Октябрь',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t\tNovember: {\r\n\t\t\t\tname: 'Ноябрь',\r\n\t\t\t\tdaysPerMounth: () => 30,\r\n\t\t\t},\r\n\t\t\tDecember: {\r\n\t\t\t\tname: 'Декабрь',\r\n\t\t\t\tdaysPerMounth: () => 31,\r\n\t\t\t},\r\n\t\t};\r\n\t}\r\n\r\n\tgetMounth(value) {\r\n\t\tfor (let key in this.mounths) {\r\n\t\t\tif (this.mounths[key].name === value) {\r\n\t\t\t\treturn this.mounths[key];\r\n\t\t\t}\r\n\t\t}\r\n\t}\r\n\r\n\tgetKey(value) {\r\n\t\tfor (let key in this.mounths) {\r\n\t\t\tif (this.mounths[key].name === value) {\r\n\t\t\t\treturn key;\r\n\t\t\t}\r\n\t\t}\r\n\t}\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (new Mounths());\r\n\n\n//# sourceURL=webpack:///./src/mounths.js?");
+
+/***/ }),
+
+/***/ "./src/selectActions.js":
+/*!******************************!*\
+  !*** ./src/selectActions.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _domconstructor__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./domconstructor */ \"./src/domconstructor.js\");\n\r\n\r\nclass selectActions {\r\n\tconstructor(selector) {\r\n\t\tthis.select = selector;\r\n\t}\r\n\r\n\tgetInstance() {\r\n\t\treturn this.select;\r\n\t}\r\n\r\n\tclearContent() {\r\n\t\tthis.select.innerHTML = '';\r\n\t}\r\n\r\n\taddOptions(options) {\r\n\t\toptions.forEach((el) => {\r\n\t\t\tthis.select.appendChild(Object(_domconstructor__WEBPACK_IMPORTED_MODULE_0__[\"generateOptions\"])('option', el));\r\n\t\t});\r\n\t}\r\n\r\n\tswitchDisable() {\r\n\t\tthis.select.disabled = false;\r\n\t}\r\n}\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (selectActions);\r\n\n\n//# sourceURL=webpack:///./src/selectActions.js?");
+
+/***/ }),
+
+/***/ "./src/timeFormatter.js":
+/*!******************************!*\
+  !*** ./src/timeFormatter.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nconst timeFormatter = (ms) => {\r\n\tlet minutes;\r\n\tlet hours;\r\n\tlet days;\r\n\tlet mounths;\r\n\tlet seconds = ms / 1000;\r\n\tif (seconds < 60) {\r\n\t\treturn { seconds: Math.round(seconds) };\r\n\t} else {\r\n\t\tminutes = Math.floor(seconds / 60);\r\n\t\tseconds = Math.floor(seconds - minutes * 60);\r\n\t\tif (minutes < 60) {\r\n\t\t\treturn { seconds, minutes };\r\n\t\t} else {\r\n\t\t\thours = Math.floor(minutes / 60);\r\n\t\t\tminutes = minutes - hours * 60;\r\n\t\t\tif (hours < 24) {\r\n\t\t\t\treturn { seconds, minutes, hours };\r\n\t\t\t} else {\r\n\t\t\t\tdays = Math.floor(hours / 24);\r\n\t\t\t\thours = hours - days * 24;\r\n\t\t\t\tif (days / 30.5) {\r\n\t\t\t\t\treturn {\r\n\t\t\t\t\t\tseconds,\r\n\t\t\t\t\t\tminutes,\r\n\t\t\t\t\t\thours,\r\n\t\t\t\t\t\tdays,\r\n\t\t\t\t\t};\r\n\t\t\t\t} else {\r\n\t\t\t\t\tmounths = Math.floor(days / 30.5);\r\n\t\t\t\t\tdays = days - mounths * 30.5;\r\n\t\t\t\t\treturn {\r\n\t\t\t\t\t\tseconds,\r\n\t\t\t\t\t\tminutes,\r\n\t\t\t\t\t\thours,\r\n\t\t\t\t\t\tdays,\r\n\t\t\t\t\t\tmounths,\r\n\t\t\t\t\t};\r\n\t\t\t\t}\r\n\t\t\t}\r\n\t\t}\r\n\t}\r\n};\r\n\r\nconst result = (ms) => {\r\n\tif (ms < 0) {\r\n\t\treturn null;\r\n\t} else {\r\n\t\treturn timeFormatter(ms);\r\n\t}\r\n};\r\n\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (result);\r\n\n\n//# sourceURL=webpack:///./src/timeFormatter.js?");
+
+/***/ }),
+
+/***/ "./src/year.js":
+/*!*********************!*\
+  !*** ./src/year.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction Year() {\r\n\tthis.year = {\r\n\t\tset year(value) {\r\n\t\t\tthis._year = value;\r\n\t\t},\r\n\t\tget year() {\r\n\t\t\treturn this._year;\r\n\t\t},\r\n\t\tget leap() {\r\n\t\t\treturn this._year % 4 === 0 ? true : false;\r\n\t\t},\r\n\t};\r\n\r\n\tObject.defineProperty(this.year, '_year', {\r\n\t\tvalue: new Date().getUTCFullYear(),\r\n\t\twritable: true,\r\n\t});\r\n}\r\n/* harmony default export */ __webpack_exports__[\"default\"] = (Year);\r\n\n\n//# sourceURL=webpack:///./src/year.js?");
+
+/***/ })
+
+/******/ });
