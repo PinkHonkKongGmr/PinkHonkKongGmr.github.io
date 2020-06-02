a8435dd62c2180c9823dd1962aadb1a79e2e2cf1
Vladimir Nickolaev <exlnted@gmail.com>
2020-06-02 22:58:46 +0300

    add pokemon agregator

---
+++ pokemonAgregator/precache-manifest.33d89e24fb559263226763ffb69f8c40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d345d9f2d8c6d8e10bb19dcbc3c500",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9ae611bb312c74f0100",
+    "url": "./static/css/main.a08255be.chunk.css"
+  },
+  {
+    "revision": "3fb7708e6abd02a8e0ba",
+    "url": "./static/js/2.da75c3c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.da75c3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ae611bb312c74f0100",
+    "url": "./static/js/main.049a7d9e.chunk.js"
+  },
+  {
+    "revision": "9a472af94198e7550f76",
+    "url": "./static/js/runtime-main.58a0cef9.js"
+  },
+  {
+    "revision": "1008be14a4bf4945b76ff93e698fc3ae",
+    "url": "./static/media/pokelogo.1008be14.png"
+  },
+  {
+    "revision": "3543dadf32e9f0b526f59c031b0c2bf9",
+    "url": "./static/media/pokemon.3543dadf.png"
+  },
+  {
+    "revision": "74cc5cd58e65672f742d7f93a665d936",
+    "url": "./static/media/pokemon.74cc5cd5.jpg"
+  }
+]);
